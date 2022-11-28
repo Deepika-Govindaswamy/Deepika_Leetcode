12aa5d057e4fa4b60a55ad365b1fe4989082574f
Deepika-Govindaswamy <118124756+Deepika-Govindaswamy@users.noreply.github.com>
2022-11-28 22:36:16 +0530

    Time: 1 ms (93.86%), Space: 42.2 MB (38.89%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String interpret(String command) {
+        
+        StringBuilder sb = new StringBuilder();
+        
+        int i = 0;
+        while( i < command.length()){
+            
+            if( command.charAt(i) == 'G') {
+                sb.append("G");
+                i++;
+            }
+            
+            else if( command.charAt(i) == '(' && command.charAt(i+1) == ')'){
+                sb.append("o");
+                i += 2;
+            }
+            
+            else{
+                sb.append("al");
+                i += 4;
+            }
+        }
+        
+        return new String(sb);
+    }
+}

fbd6415e31965cfed8e3e991c2098d031d37dc21
Deepika-Govindaswamy <118124756+Deepika-Govindaswamy@users.noreply.github.com>
2024-12-29 21:01:09 +0000

    Time: 27 ms (7.29%) | Memory: 45.6 MB (58.28%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.java
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.java
@@ -6,10 +6,10 @@ class Solution {
         System.out.println(Arrays.toString(nums));
 
         int i = nums.length - 1;
-        if (nums[0] > 0){
-            return nums[i] * nums[i-1] * nums[i-2];
-        }
-        else{
+        // if (nums[0] > 0){
+        //     return nums[i] * nums[i-1] * nums[i-2];
+        // }
+       
             if (nums[0] < 0 && nums[i] > 0){
 
                 if ((nums[0] * nums[1]) < (nums[i-1] * nums[i-2])){
@@ -19,7 +19,7 @@ class Solution {
                     return nums[0] * nums[1] * nums[i];
                 }
             }
-        }
+        
         return nums[i] * nums[i-1] * nums[i-2];
     }
 }

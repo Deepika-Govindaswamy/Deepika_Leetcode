33e7b5f61102d1c2430b6d7ed1c96ca9da46ca11
Deepika <118124756+Deepika-Govindaswamy@users.noreply.github.com>
2025-06-20 20:24:21 +0100

    Time: 10 ms (1.62%) | Memory: 41.6 MB (7.66%) - LeetSync

---
+++ 1446-angle-between-hands-of-a-clock/angle-between-hands-of-a-clock.java
@@ -0,0 +1,30 @@
+class Solution {
+    public double angleClock(int hour, int minutes) {
+        
+        // Degrees covered by hours hand and minutes hand 
+        // int hourDegree = 30;
+        // int minutesDegree = 6;
+
+        double hoursMoved = hour * 30;
+        double minutesMoved = minutes * 6; 
+
+        double minutesPosition = (12 + (double)minutes / 5) % 12;
+
+        // the degree hours had moved by the time minutes hand covered minutes degree
+        double hoursMovedInMinutes = ((double) minutes/60) * 30;
+
+        System.out.println (hoursMoved + " " + minutesMoved + " " + hoursMovedInMinutes);
+
+        double degree = Math.abs (hoursMoved - minutesMoved);
+        if (degree <= 0 || minutesPosition < hour){
+
+            degree += hoursMovedInMinutes;
+        } 
+
+        else{
+            degree = Math.abs (degree - hoursMovedInMinutes);
+        }
+
+        return Math.min (degree, 360 - degree);
+    }
+}

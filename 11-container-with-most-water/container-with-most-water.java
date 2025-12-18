8ec40b7c79cd89f88b7a18ab18a5b52afdead4b1
Deepika <118124756+Deepika-Govindaswamy@users.noreply.github.com>
2025-12-18 19:19:14 +0530

    Time: 6 ms (6.08%) | Memory: 77.3 MB (31.08%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -6,17 +6,9 @@ class Solution {
         while (s < e) {
 
             int h = Math.min (height[s], height[e]);
-
-            System.out.println ("h: " + h);
-
             int w = Math.abs (e - s);
-
-            System.out.println ("w: " + w);
-
             vol = Math.max (vol, h * w);
-
-            System.out.println ("vol: " + vol);
-
+            
             if (height[s] < height[e]) {
                 s++;
             }
